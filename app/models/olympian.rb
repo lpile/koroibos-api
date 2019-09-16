89c00ac3dbdfedaf38018b78c16cdcf2c8838682
lpile <logan.pile@gmail.com>
2019-09-16 13:39:53 -0600

    Add instance methods sport and total_medals_won to Olympian model with specs

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -4,4 +4,13 @@ class Olympian < ApplicationRecord
   has_many :results
   has_many :olympics, through: :results
   has_many :events, through: :results
+
+  def sport
+    sport_id = self.events.first.sport_id
+    Sport.find_by(id: sport_id).name
+  end
+
+  def total_medals_won
+    results.where("medal != 'NA'").count
+  end
 end
