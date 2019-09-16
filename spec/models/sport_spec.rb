32290661a74a6bc920a40ddb8b47eac8e127a7b5
lpile <logan.pile@gmail.com>
2019-09-16 11:17:42 -0600

    Add has many Events association to Sport model

--- spec/models/sport_spec.rb
+++ spec/models/sport_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Sport, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
   end
+
+  describe 'associations' do
+    it { should have_many(:events) }
+  end
 end
