c1c4420d580aa4e0fef937d4829c347c11a868d1
lpile <logan.pile@gmail.com>
2019-09-16 11:13:32 -0600

    Add validations test block to Sport model spec

--- spec/models/sport_spec.rb
+++ spec/models/sport_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Sport, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
 end
